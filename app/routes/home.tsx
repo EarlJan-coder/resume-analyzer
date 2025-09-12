acafebea04a118b4d49d8cf30db428e73618d697
EarlJan-coder <earljhonamalatag2@gmail.com>
2025-09-12 23:39:09 +0800

    Landing page design(no functionalities)

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/Components/Navbar";
+import {resumes} from "../../Constants";
+import ResumeCard from "~/Components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVGenius" },
+    { name: "description", content: "Transform your resume with instant AI analysis. Get personalized feedback on formatting, keywords, and ATS compatibility to boost your interview chances and land your dream job faster." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className={'bg-[url(/images/bg-main.svg)] bg-cover'}>
+    <Navbar/>
+      {window.puter.ai.chat()}
+
+      <section className={'main-section'}>
+        <div className={'page-heading py-16'}>
+            <h1>Your career breakthrough starts here.</h1>
+            <h2>Review your CV and check with an AI powered feedbacks!</h2>
+        </div>
+          {resumes.length > 0 &&(
+              <div className={'resumes-section'}>
+                  {resumes.map((resume) =>(
+                      <ResumeCard key={resume.id} resume={resume}/>
+                  ))}
+              </div>
+          )}
+    </section>
+
+
+  </main>;
 }
